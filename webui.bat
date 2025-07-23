df5d15a89739724611d9e1b826edb8896ce1a0e3
Golden Li <fuhao.li@foxmail.com>
2025-07-23 15:24:05 +0800

    c

--- webui.bat
+++ webui.bat
@@ -4,4 +4,4 @@ echo ***** Current directory: %CURRENT_DIR% *****
 set PYTHONPATH=%CURRENT_DIR%
 
 rem set HF_ENDPOINT=https://hf-mirror.com
-streamlit run .\webui\Main.py --browser.gatherUsageStats=False --server.enableCORS=True
+python -m streamlit run .\webui\Main.py --browser.gatherUsageStats=False --server.enableCORS=True
